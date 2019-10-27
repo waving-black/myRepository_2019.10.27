1a6fc0043d38cd6e32d4b1747d25da7ae7b40814
waving-black <1274646379@qq.com>
2019-10-27 19:00:35 +0800

    修改myTest1,判断字符串是否为空

--- src/main/java/com/waving/myTest1.java
+++ src/main/java/com/waving/myTest1.java
@@ -1,13 +1,17 @@
 package com.waving;
 
+import java.util.Optional;
+
 /**
  * @Author waving
  * @Date 2019/10/27
  */
 public class myTest1 {
     public static void main(String[] args) {
+        String str = "good night!";
         System.out.println("I will work tomorrow!");
         System.out.println("good luck!");
+        Optional.ofNullable(str).orElse("").length();
     }
     
     public void method(){
