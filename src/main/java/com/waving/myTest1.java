98bd5437c35e70d59f0587152897d1ea3e4c9286
waving-black <1274646379@qq.com>
2019-10-27 16:45:07 +0800

    新增myTest1.java文件

---
+++ src/main/java/com/waving/myTest1.java
@@ -0,0 +1,11 @@
+package com.waving;
+
+/**
+ * @Author waving
+ * @Date 2019/10/27
+ */
+public class myTest1 {
+    public static void main(String[] args) {
+        System.out.println("I will work tomorrow!");
+    }
+}
