48920c43e6944fdd56d8cc71cc236fa89e327f06
waving-black <1274646379@qq.com>
2019-10-27 17:44:31 +0800

    修改myTest1

--- src/main/java/com/waving/myTest1.java
+++ src/main/java/com/waving/myTest1.java
@@ -7,5 +7,6 @@ package com.waving;
 public class myTest1 {
     public static void main(String[] args) {
         System.out.println("I will work tomorrow!");
+        System.out.println("good luck!");
     }
 }
